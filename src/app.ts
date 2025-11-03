d3d47b95dc7aa68d62bfdcc87a7fac1d41b63df9
Mavi <mariapimenta2607@gmail.com>
2025-11-03 19:36:17 -0300

    (fix): fix routes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import userRouter from "./routes/user.router";
 import authRouter from "./routes/auth.router";
 import { handleError } from "./middlewares/handle.middleware";
+import taskRouter from "./routes/task.router";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors());
 
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
+app.use("/tasks", taskRouter);
 
 app.use(handleError);
 
